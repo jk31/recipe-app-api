d6ed16898d543c4fd3a9ddc2685d2fa067919e33
JK <jarekkornowicz@googlemail.com>
2020-04-09 14:13:47 +0200

    added custo mrecipe view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,10 +23,16 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serialize a recipe"""
     ingredients = serializers.PrimaryKeyRelatedField(many=True, queryset=Ingredient.objects.all())
     tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())
-    
+
 
     class Meta:
         model = Recipe
         fields = ("id", "title", "ingredients", "tags", "time_minutes", "price", "link")
         read_only_fields = ("id",)
 
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    
